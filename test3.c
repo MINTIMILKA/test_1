a04137b8d83c105265277994c33e0f522b8644bf
user_1 <kjkj0581@naver.com>
2025-02-13 11:22:28 +0900

    test_4 added

---
+++ test3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	printf("github_test: test1.c");
+	printf("github_test: test2.c");
+	printf("github_test: test3.c");
+
+	return 0;
+}
+
---
+++ test4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	printf("test4.c");
+	printf("test4.c");
+
+	return 0;
+}
+
